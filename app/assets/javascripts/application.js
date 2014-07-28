18bae3342335af00745e734f1e2d5bcccb73c39c
Dave Laird <kiwiupover@gmail.com>
2014-07-27 23:00:37 -0700

    Removed unused variables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,42 +15,33 @@
 //= require turbolinks
 //= require_tree .
 
-var  calendar = (function(argument) {
+var calendar = (function(argument) {
   'use-strict';
 
-  var calData = calendarEvents;
   var calContainer = $('.calendar-wrapper');
-  var calTable = $('.calendar-wrapper').find('table');
-  var calTableHeight = calTable.outerHeight();
+  var calTable = calContainer.find('table');
   var calHeader = calTable.find('th').outerHeight();
   var calTableOffset = calTable.offset();
   var calTimeSlots = 48;
   var calTimeSlotsHeight = calTable.outerHeight() - calHeader;
   var pxPerSlot = calTimeSlotsHeight / calTimeSlots;
   var dayWidth = calTable.find('th.day-header:first').width();
-  var dayWidthOffset = calTable.find('th:first').outerWidth();
-  var dayTopOffset = calTable.find('th:first').outerHeight();
-  var beginningTime = new Date("01/01/2014 " + "00:00").getTime();
-
-  var MILLS_IN_DAY = 86400000;
-  var MILLS_PER_TIME_SLOT = MILLS_IN_DAY / 48;
+  var borders = 2;
 
   var weekDays = {
-    Sunday: calTable.find('th:nth-child(2)').offset().left -  calTableOffset.left + 1,
-    Monday: calTable.find('th:nth-child(3)').offset().left -  calTableOffset.left + 1,
-    Tuesday: calTable.find('th:nth-child(4)').offset().left -  calTableOffset.left + 1,
-    Wednesday: calTable.find('th:nth-child(5)').offset().left -  calTableOffset.left + 1,
-    Thursday: calTable.find('th:nth-child(6)').offset().left -  calTableOffset.left + 1,
-    Friday: calTable.find('th:nth-child(7)').offset().left -  calTableOffset.left + 1,
-    Saturday: calTable.find('th:nth-child(8)').offset().left -  calTableOffset.left + 1,
+    Sunday: calTable.find('th:nth-child(2)').offset().left -  calTableOffset.left + borders,
+    Monday: calTable.find('th:nth-child(3)').offset().left -  calTableOffset.left + borders,
+    Tuesday: calTable.find('th:nth-child(4)').offset().left -  calTableOffset.left + borders,
+    Wednesday: calTable.find('th:nth-child(5)').offset().left -  calTableOffset.left + borders,
+    Thursday: calTable.find('th:nth-child(6)').offset().left -  calTableOffset.left + borders,
+    Friday: calTable.find('th:nth-child(7)').offset().left -  calTableOffset.left + borders,
+    Saturday: calTable.find('th:nth-child(8)').offset().left -  calTableOffset.left + borders,
   };
 
-
-
   var makeEvent = function(event){
-
     var start_time = new Date("01/01/2014 " + event.start_time).getTime();
     var end_time = new Date("01/01/2014 " + event.end_time).getTime();
+    var beginningTime = new Date("01/01/2014 " + "00:00").getTime();
     var startSlot = (start_time - beginningTime) / 1000 / 60 / 60;
     var timeLength = (end_time - start_time) / 1000 / 60 / 60;
 
@@ -65,8 +56,8 @@ var  calendar = (function(argument) {
   };
 
   return {
-    init: function(){
-      calData.forEach(function(event){
+    init: function(events){
+      events.forEach(function(event){
         $(calContainer).append(makeEvent(event));
       });
     }
@@ -74,5 +65,5 @@ var  calendar = (function(argument) {
 
 })();
 
-calendar.init();
+calendar.init(calendarEvents);
 
