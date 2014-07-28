22b0a527ece7869ebc284414e3cfad4f13cb5693
Dave Laird <kiwiupover@gmail.com>
2014-07-27 22:48:17 -0700

    add js to render the calendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,65 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var  calendar = (function(argument) {
+  'use-strict';
+
+  var calData = calendarEvents;
+  var calContainer = $('.calendar-wrapper');
+  var calTable = $('.calendar-wrapper').find('table');
+  var calTableHeight = calTable.outerHeight();
+  var calHeader = calTable.find('th').outerHeight();
+  var calTableOffset = calTable.offset();
+  var calTimeSlots = 48;
+  var calTimeSlotsHeight = calTable.outerHeight() - calHeader;
+  var pxPerSlot = calTimeSlotsHeight / calTimeSlots;
+  var dayWidth = calTable.find('th.day-header:first').width();
+  var dayWidthOffset = calTable.find('th:first').outerWidth();
+  var dayTopOffset = calTable.find('th:first').outerHeight();
+  var beginningTime = new Date("01/01/2014 " + "00:00").getTime();
+
+  var MILLS_IN_DAY = 86400000;
+  var MILLS_PER_TIME_SLOT = MILLS_IN_DAY / 48;
+
+  var weekDays = {
+    Sunday: calTable.find('th:nth-child(2)').offset().left -  calTableOffset.left + 1,
+    Monday: calTable.find('th:nth-child(3)').offset().left -  calTableOffset.left + 1,
+    Tuesday: calTable.find('th:nth-child(4)').offset().left -  calTableOffset.left + 1,
+    Wednesday: calTable.find('th:nth-child(5)').offset().left -  calTableOffset.left + 1,
+    Thursday: calTable.find('th:nth-child(6)').offset().left -  calTableOffset.left + 1,
+    Friday: calTable.find('th:nth-child(7)').offset().left -  calTableOffset.left + 1,
+    Saturday: calTable.find('th:nth-child(8)').offset().left -  calTableOffset.left + 1,
+  };
+
+
+
+  var makeEvent = function(event){
+
+    var start_time = new Date("01/01/2014 " + event.start_time).getTime();
+    var end_time = new Date("01/01/2014 " + event.end_time).getTime();
+    var startSlot = (start_time - beginningTime) / 1000 / 60 / 60;
+    var timeLength = (end_time - start_time) / 1000 / 60 / 60;
+
+    var width = dayWidth;
+    var height = pxPerSlot * (timeLength * 2);
+    var top = pxPerSlot * (startSlot * 2) + calHeader;
+
+    return $('<div>')
+      .addClass('event')
+      .css({width: width, height: height, top: top, left: weekDays[event.day]})
+      .text(event.name);
+  };
+
+  return {
+    init: function(){
+      calData.forEach(function(event){
+        $(calContainer).append(makeEvent(event));
+      });
+    }
+  };
+
+})();
+
+calendar.init();
+
--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,8 +3,19 @@ class CalendarsController < ActionController::Base
 
   def index
     @events = [
-      { name: 'Yoga', day: 'Sunday', start_time: '9:00', end_time: '10:00' },
-      { name: 'Running', day: 'Monday', start_time: '11:00', end_time: '12:30' }
+      { name: 'Start', day: 'Sunday', start_time: '00:00', end_time: '10:00' },
+      { name: 'Yoga', day: 'Sunday', start_time: '11:00', end_time: '13:00' },
+      { name: 'Dancing', day: 'Sunday', start_time: '22:00', end_time: '23:30' },
+      { name: 'Running', day: 'Monday', start_time: '11:00', end_time: '12:30' },
+      { name: 'Swimming', day: 'Tuesday', start_time: '11:30', end_time: '12:30' },
+      { name: 'Running', day: 'Wednesday', start_time: '08:30', end_time: '12:00' },
+      { name: 'Running', day: 'Thursday', start_time: '06:00', end_time: '7:30' },
+      { name: 'Swimming', day: 'Thursday', start_time: '09:00', end_time: '10:30' },
+      { name: 'Running', day: 'Friday', start_time: '11:00', end_time: '12:30' },
+      { name: 'Running', day: 'Saturday', start_time: '11:00', end_time: '12:30' },
+      { name: 'Dancing', day: 'Tuesday', start_time: '22:00', end_time: '23:30' },
+      { name: 'Dancing', day: 'Friday', start_time: '22:00', end_time: '23:30' },
+      { name: 'Dancing', day: 'Saturday', start_time: '22:00', end_time: '23:30' },
     ]
   end
 end
